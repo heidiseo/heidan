fa8a19f4181f992c4f8496b98819ccf118cac5a5
Heidi Seo <heidiseo1@gmail.com>
2019-09-10 17:50:53 +0100

    endpoint jokes

--- main.go
+++ main.go
@@ -1,16 +1,18 @@
 package main
 
 import (
-	"bytes"
-	"log"
+	"fmt"
+	"io/ioutil"
 	"net/http"
-	"os"
-	"strconv"
 
-	"github.com/gin-gonic/gin"
 	_ "github.com/heroku/x/hmetrics/onload"
-	"github.com/russross/blackfriday"
 )
+
+/*type Joke struct {
+	msg string
+}
+
+type Jokes []Joke
 
 func repeatHandler(r int) gin.HandlerFunc {
 	return func(c *gin.Context) {
@@ -20,40 +22,68 @@ func repeatHandler(r int) gin.HandlerFunc {
 		}
 		c.String(http.StatusOK, buffer.String())
 	}
-}
+}*/
 
 func main() {
-	port := os.Getenv("PORT")
-
-	if port == "" {
-		log.Fatal("$PORT must be set")
+	client := &http
+	response, err := http.Get("https://icanhazdadjoke.com")
+	response.Header.Add()
+headers:
+	{
+	Accept:
+		"application/json"
 	}
-
-	tStr := os.Getenv("REPEAT")
-	repeat, err := strconv.Atoi(tStr)
 	if err != nil {
-		log.Printf("Error converting $REPEAT to an int: %q - Using default\n", err)
-		repeat = 5
+		fmt.Printf("The HTTP request failed with error %s\n", err)
+	} else {
+		data, _ := ioutil.ReadAll(response.Body)
+		fmt.Println(string(data))
 	}
 
-	router := gin.New()
-	router.Use(gin.Logger())
-	router.LoadHTMLGlob("templates/*.tmpl.html")
-	router.Static("/static", "static")
+	/*port := os.Getenv("PORT")
 
-	router.GET("/", func(c *gin.Context) {
-		c.HTML(http.StatusOK, "index.tmpl.html", nil)
-	})
+		if port == "" {
+			log.Fatal("$PORT must be set")
+		}
 
-	router.GET("/mark", func(c *gin.Context) {
-		c.String(http.StatusOK, string(blackfriday.Run([]byte("**hi!**"))))
-	})
+		tStr := os.Getenv("REPEAT")
+		repeat, err := strconv.Atoi(tStr)
+		if err != nil {
+			log.Printf("Error converting $REPEAT to an int: %q - Using default\n", err)
+			repeat = 5
+		}
 
-	router.GET("/test", func(c *gin.Context) {
-		c.String(http.StatusOK, string(blackfriday.Run([]byte("**test**"))))
-	})
+		router := gin.New()
+		router.Use(gin.Logger())
+		router.LoadHTMLGlob("templates/*.tmpl.html")
+		router.Static("/static", "static")
 
-	router.GET("/repeat", repeatHandler(repeat))
+		router.GET("/", func(c *gin.Context) {
+			c.HTML(http.StatusOK, "index.tmpl.html", nil)
+		})
 
-	router.Run(":" + port)
+		router.GET("/mark", func(c *gin.Context) {
+			c.String(http.StatusOK, string(blackfriday.Run([]byte("**hi!**"))))
+		})
+
+		router.GET("/test", func(c *gin.Context) {
+			c.String(http.StatusOK, string(blackfriday.Run([]byte("**hi!**"))))
+		})
+
+		router.GET("/repeat", repeatHandler(repeat))
+
+		router.GET("/jokes", showJokes())
+	)
+
+		router.Run(":" + port)*/
 }
+
+/*func showJokes(w http.ResponseWriter, r *http.Request) {
+	ctx := r.Body()
+	logging.Info(ctx, logging.Data{}, jokes accessed)
+    jokes := Jokes{
+        Joke{msg: ctx.msg},
+    }
+    json.NewEncoder(w).Encode(jokes)
+}
+*/
