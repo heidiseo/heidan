af27e49b0445ae8d1e1a31f475045fc73c6a7eca
heidiseo <heidiseo1@gmail.com>
2019-10-04 21:36:04 +0100

    add functions to add, update and delete data from Firestore

--- main.go
+++ main.go
@@ -3,11 +3,11 @@ package main
 import (
 	"context"
 	"fmt"
+	"google.golang.org/api/iterator"
 	"log"
 
 	"cloud.google.com/go/firestore"
 	firebase "firebase.google.com/go"
-	"google.golang.org/api/iterator"
 	"google.golang.org/api/option"
 )
 
@@ -24,15 +24,19 @@ func main() {
 	if err != nil {
 		log.Fatalln(err)
 	}
-	ReadFromFirestore(ctx, client)
-	AddToFirestore(ctx, client)
-	//ReadFromFirestore(ctx, client)
-
+	ReadFromFirestore(client)
+	AddToFirestore(client)
+	ReadFromFirestore(client)
+	UpdateInFirestore(client)
+	ReadFromFirestore(client)
+	DeleteFromFirestore(client)
+	ReadFromFirestore(client)
 	defer client.Close()
 }
 
 //ReadFromFirestore function will read data from firestore
-func ReadFromFirestore(ctx context.Context, c *firestore.Client) {
+func ReadFromFirestore(c *firestore.Client) error {
+	ctx := context.Background()
 	iter := c.Collection("adventures").Documents(ctx)
 	for {
 		doc, err := iter.Next()
@@ -40,18 +44,51 @@ func ReadFromFirestore(ctx context.Context, c *firestore.Client) {
 			break
 		}
 		if err != nil {
-			log.Fatalf("Failed to iterate: %v", err)
+			return err
 		}
 		fmt.Println(doc.Data())
 	}
+	return nil
 }
 
 //AddToFirestore will add data to firestore
-func AddToFirestore(ctx context.Context, c *firestore.Client) {
+func AddToFirestore(c *firestore.Client) error {
+	ctx := context.Background()
 	_, _, err := c.Collection("adventures").Add(ctx, map[string]interface{}{
-		"concert": "Jon Bellion",
+		"type":    "libraries",
+		"name": "British Library",
+		"location": "London King's Cross",
+		"price": 0,
+		"estimated_duration": "2 hours",
 	})
 	if err != nil {
-		log.Fatalf("Failed adding alovelace: %v", err)
+		// Handle any errors in an appropriate way, such as returning them.
+		log.Printf("An error has occurred: %s", err)
+		return err
 	}
+	return nil
+}
+
+func UpdateInFirestore(c *firestore.Client) error {
+	ctx := context.Background()
+	_, err := c.Collection("adventures").Doc("activities").Set(ctx, map[string]interface{}{
+		"priority": "boom boom",
+	}, firestore.MergeAll)
+	if err != nil {
+		// Handle any errors in an appropriate way, such as returning them.
+		log.Printf("An error has occurred: %s", err)
+		return err
+	}
+	return nil
+}
+
+func DeleteFromFirestore(c *firestore.Client) error {
+	ctx := context.Background()
+	_, err := c.Collection("adventures").Doc("activities").Delete(ctx)
+	if err != nil {
+		// Handle any errors in an appropriate way, such as returning them.
+		log.Printf("An error has occurred: %s", err)
+		return err
+	}
+	return nil
 }
